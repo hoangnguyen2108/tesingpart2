2f8ff4f4616423293b453401b74884896d4ed104
hoangnguyen2108 <langtu092000@gmail.com>
2020-10-24 15:31:04 -0400

    updated for heroku week 6

---
+++ params/params.js
@@ -0,0 +1,5 @@
+const DATABASECONNECTION = "mongodb+srv://langtu092000:longbadao@hoang.sba8w.mongodb.net/langtu092000?retryWrites=true&w=majority"
+
+module.exports = {
+    DATABASECONNECTION 
+}
--- routes/web/home.js
+++ routes/web/home.js
@@ -3,6 +3,13 @@ var express = require("express");
 
 var router = express.Router();
 
+var passport = require("passport");
+
+var ensureAuthenticated = require("../../auth/auth").ensureAuthenticated;
+
+var User = require("../../models/user");
+
+var router = express.Router();
 
 router.get("/", function(req, res) {
     // console.log("hello I'm on the start page");
@@ -16,6 +23,9 @@ router.get("/", function(req, res) {
  router.get("/about", function(req, res){
     res.render("home/about");
  });
+
+ 
+
 //Get contact page
  router.get("/contact", function(req, res){
     res.render("home/contact");
@@ -29,4 +39,72 @@ router.get("/", function(req, res) {
     res.render("home/project");
  });
 
- module.exports = router;
+ //Get  login page
+ router.get("/login", function(req, res){
+   res.render("home/login");
+});
+//get logout page
+router.get("/logout", function(req, res){
+   req.logout();
+   res.redirect("/home");
+});
+
+router.get("/signup", function (req, res) {
+   res.render("home/signup")
+});
+
+
+
+
+
+
+
+
+router.get("/login", function (req, res) {
+   res.render("home/login")
+});
+
+router.get("/logout", function(req, res){
+   req.logout();
+   res.redirect("/home");
+});
+
+router.post("/login", passport.authenticate("login", {
+   successRedirect: "/",
+   failureRedirect: "/login",
+   failureFlash: true
+}));
+
+router.get("/signup", function (req, res) {
+   res.render("home/signup")
+});
+
+router.post("/signup", function (req, res, next) {
+   var username = req.body.username;
+   var email = req.body.email;
+   var password = req.body.password;
+
+   User.findOne({ email: email }, function (err, user) {
+      if (err) { return next(err); }
+      if (user) {
+         req.flash("error", "There's already an account with this email");
+         return res.redirect("/signup");
+      }
+
+      var newUser = new User({
+         username: username,
+         password: password,
+         email: email
+      });
+
+      newUser.save(next);
+
+   });
+
+}, passport.authenticate("login", {
+   successRedirect: "/",
+   failureRedirect: "/signup",
+   failureFlash: true
+}));
+
+module.exports = router;
